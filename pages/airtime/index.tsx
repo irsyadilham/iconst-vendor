93c7ae1cc799ebe40d0ec9e8baad7b036ae765b5
irsyadilham <irsyadmhdilham@gmail.com>
2023-02-19 07:58:00 +0800

    update 19/2/23

--- pages/airtime/index.tsx
+++ pages/airtime/index.tsx
@@ -4,10 +4,13 @@ import Back from '../../components/back';
 import { get, post } from '../../functions/fetch';
 import { gsap } from 'gsap';
 
-interface airtime {
+type airtime = {
   id?: number;
   validity: number;
   price: string;
+  promo_active: boolean;
+  promo_price: string;
+  hide: boolean;
 }
 
 export default function CoinTopup() {
@@ -52,12 +55,14 @@ export default function CoinTopup() {
       cancelPurchase();
       context.loading.dispatch({type: 'OFF'});
     } catch (err) {
-      if (!err.ok) {
-        console.log('error = ', await err.json());
-      }
-      cancelPurchase();
-      alert('Purchase unsuccessful, please try again later');
       context.loading.dispatch({type: 'OFF'});
+      cancelPurchase();
+      if (err.status === 400) {
+        const data = await err.json();
+        alert(data.message);
+        return;
+      }
+      alert('Purchase unsuccessful, please try again later');
     }
   }
 
@@ -73,13 +78,27 @@ export default function CoinTopup() {
       <p className="mt-1 text-sm">Select option below</p>
 
       <section className="mt-1 space-y-[1.2em]">
-        {airtimes.map((airtime, i) => {
+        {airtimes.filter(val => !val.hide).map((airtime, i) => {
           return (
             <button onClick={() => selectAirtime(airtime)} key={i} className="flex w-full justify-between items-center bg-white shadow-normal p-[1.5em] rounded-md">
 
               <h4 className="text-gray">{airtime.validity} days</h4>
 
-              <h4 className="text-primary">RM{airtime.price}</h4>
+              {(() => {
+                if (airtime.promo_active) {
+                  return (
+                    <div className="flex flex-col">
+                      <div className="relative">
+                        <p className="text-xs text-left">RM{airtime.price}</p>
+                        <div className="w-10/12 h-[2px] bg-red-600 rotate-[8deg] absolute -left-[.5em] top-[.5em]"/>
+                      </div>
+                      <h4 className="text-primary">RM{airtime.promo_price}</h4>
+                    </div>
+                  )
+                } else {
+                  return <h4 className="text-primary">RM{airtime.price}</h4>;
+                }
+              })()}
 
             </button>
           );
@@ -93,7 +112,7 @@ export default function CoinTopup() {
           <h3 className="text-lg text-primary">Confirm purchase</h3>
           {(() => {
             if (selectedAirtime) {
-              return <p className="mt-[.7em]">Are you sure to purchase an airtime validity <span className="font-bold text-primary">{selectedAirtime.validity} days</span> for <span className="font-bold text-primary">RM{selectedAirtime.price}</span></p>;
+              return <p className="mt-[.7em]">Are you sure to purchase an airtime validity <span className="font-bold text-primary">{selectedAirtime.validity} days</span> for <span className="font-bold text-primary">RM{selectedAirtime.promo_active ? selectedAirtime.promo_price : selectedAirtime.price}</span></p>;
             }
           })()}
 
