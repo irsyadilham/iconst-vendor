69551c9637937474216626f03fc24789430d842e
irsyadilham <irsyadmhdilham@gmail.com>
2023-02-22 07:05:49 +0800

    update airtime/thank-you page

--- pages/airtime/thank-you.tsx
+++ pages/airtime/thank-you.tsx
@@ -10,7 +10,7 @@ export default function ThankYou() {
   const counter = useRef<HTMLSpanElement>(null);
   
   const closeWindow = () => {
-    window.close();
+    router.push('/jobs');
   }
 
   const submitResult = async () => {
